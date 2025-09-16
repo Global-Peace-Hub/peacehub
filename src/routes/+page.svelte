bd7a56a248ce660793202756f06c4e2d45712479
tvancisin <tomasvancisin@gmail.com>
2025-09-16 16:13:13 +0100

    citations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -126,6 +126,23 @@
             }
         });
     }
+
+    let show = false;
+    const toggle = () => (show = !show);
+    function openBibtex() {
+        const bibtex = `@dataset{peter2025mend,
+  author       = {Peter, Mateja and Badanjak, Sanja and D'Amico, Elisa and Houghton, Kasia},
+  title        = {Mediation Event and Negotiators Database (MEND)},
+  year         = {2025},
+  publisher    = {Harvard Dataverse},
+  version      = {V1},
+  doi          = {10.7910/DVN/PYRHS6},
+  url          = {https://doi.org/10.7910/DVN/PYRHS6}
+}`;
+        const newWin = window.open("", "_blank");
+        newWin.document.write("<pre>" + bibtex + "</pre>");
+        newWin.document.close();
+    }
 </script>
 
 <div id="wrapper" bind:clientWidth={width}>
@@ -238,6 +255,52 @@
                     src="peacerep_logo.jpg"
                 />
             </div>
+            <button class="cite" on:click={toggle} title="Cite"
+                ><i class="fa fa-quote-right" aria-hidden="true"></i>
+            </button>
+            <div class="cite_div {show ? 'visible' : ''}">
+                <p>
+                    <strong>MLA</strong><br />
+                    Peter, Mateja, et al.
+                    <em>Mediation Event and Negotiators Database (MEND)</em>.
+                    Harvard Dataverse, V1, 2025,
+                    https://doi.org/10.7910/DVN/PYRHS6.
+                </p>
+
+                <p>
+                    <strong>APA</strong><br />
+                    Peter, M., Badanjak, S., D'Amico, E., &amp; Houghton, K. (2025).
+                    <em>Mediation Event and Negotiators Database (MEND)</em> (V1).
+                    Harvard Dataverse. https://doi.org/10.7910/DVN/PYRHS6
+                </p>
+
+                <p>
+                    <strong>Chicago</strong><br />
+                    Peter, Mateja, Sanja Badanjak, Elisa D'Amico, and Kasia Houghton.
+                    2025.
+                    <em>Mediation Event and Negotiators Database (MEND)</em>.
+                    V1. Harvard Dataverse. https://doi.org/10.7910/DVN/PYRHS6.
+                </p>
+
+                <p>
+                    <strong>Harvard</strong><br />
+                    Peter, M., Badanjak, S., D'Amico, E. and Houghton, K., 2025.
+                    <em>Mediation Event and Negotiators Database (MEND)</em>.
+                    V1. Harvard Dataverse. Available at:
+                    &lt;https://doi.org/10.7910/DVN/PYRHS6&gt;.
+                </p>
+
+                <p>
+                    <strong>Vancouver</strong><br />
+                    1. Peter M, Badanjak S, D'Amico E, Houghton K. Mediation Event
+                    and Negotiators Database (MEND) [dataset]. Harvard Dataverse;
+                    2025. Version 1. Available from: https://doi.org/10.7910/DVN/PYRHS6
+                </p>
+                <strong>BibTeX</strong><br />
+                <button class="bib-button" on:click={openBibtex}>
+                    View BibTeX
+                </button>
+            </div>
         </header>
     </div>
 
@@ -285,16 +348,6 @@
     </section>
 
     <footer id="credit">
-        <div style="width: 80%; margin: auto">
-            <p style="text-align: center; margin-bottom: 5px">
-                <strong>Cite Data as:</strong> Peter, Mateja; Badanjak, Sanja;
-                D'Amico, Elisa; Houghton, Kasia, 2025,<br /> "Mediation Event
-                and Negotiators Database (MEND)", Harvard Dataverse, V1,<br />
-                <a href="https://doi.org/10.7910/DVN/PYRHS6" target="_blank"
-                    >https://doi.org/10.7910/DVN/PYRHS6</a
-                >
-            </p>
-        </div>
         <p style="text-align: center; margin-bottom: 5px">
             <strong> Web and Visualization Development: </strong>
             <a href="https://tomasvancisin.co.uk/" target="_blank">
@@ -346,6 +399,67 @@
         margin: 0px;
     }
 
+    .cite {
+        position: absolute;
+        top: 5px;
+        right: 5px;
+        background-color: #ffffff; /* Green */
+        border: none;
+        border-radius: 3px;
+        color: black;
+        padding: 8px 15px;
+        text-align: center;
+        text-decoration: none;
+        font-size: 16px;
+        cursor: pointer;
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    }
+
+    .cite:focus {
+        outline: 0px solid #005fcc;
+        outline-offset: 1px;
+        position: absolute;
+        z-index: 1;
+    }
+
+    .cite_div {
+        position: absolute;
+        padding: 20px;
+        text-align: left;
+        font-size: 12px;
+        top: 45px;
+        right: 5px;
+        width: 300px;
+        border-radius: 3px;
+        background-color: #000000;
+        transform: translateY(-120%);
+        transition: transform 0.4s ease;
+        z-index: 999;
+        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    }
+
+    .cite_div.visible {
+        transform: translateY(0);
+    }
+
+    .bib-button {
+        background: none;
+        border: none;
+        color: white;
+        text-decoration: underline;
+        cursor: pointer;
+        padding: 0;
+        font: inherit;
+    }
+
+    .bib-button:hover,
+    .bib-button:focus {
+        outline: none;
+        background-color: rgb(255, 255, 255);
+        color: black;
+        border-radius: 2px;
+    }
+
     #home {
         position: relative;
         width: 100%;
@@ -366,8 +480,8 @@
 
     #navigation {
         position: absolute;
-        top: 0px;
-        left: 0px;
+        top: 5px;
+        left: 5px;
         display: flex;
         align-items: center;
         gap: 3px;
